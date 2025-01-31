e37a20cd0501356f5378c502b44b85862f1fc1a7
FiNelSa <finelsa97@gmail.com>
2025-02-01 00:55:09 +0300

    java

--- Blackjack/src/Main.java
+++ Blackjack/src/Main.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 import java.util.Random;
         public class Main {
             public static void main(String[] args) {
-                ArrayList<Integer> user = new ArrayList<>();
-                ArrayList<Integer> bank = new ArrayList<>();
+                ArrayList<Integer> user  = new ArrayList<>();
+                ArrayList<Integer> user2 = new ArrayList<>();
+                ArrayList<Integer> bank  = new ArrayList<>();
                 Scanner scan = new Scanner(System.in);
                 Random rand = new Random();
                 boolean g = true;
-                boolean b;
-                int money = 1000;
-                int betm;
+                int b;
+                double money = 1000;
+                double betm;
                 int a;//Daha sonra kullanmak için değişkenler
                 int[] f = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};  //Gelebilecek sayıların listesi
                 while(g) {  //Tekrar oynanabilmesi için
@@ -30,9 +31,9 @@ import java.util.Random;
                     System.out.println("Sizin eliniz      : " + user);   //Oyuncuya kendi kartlarını söyler
                     System.out.println("Kurpiyerin eliniz : " + bank.getFirst() + " * ");               //Kurpiyerin 1. kartını söyler ama 2. kartını söylemez
                     while (user.size() < 5 & t < 21) { //Oyuncunun elindeki kart 5'ten küçük iken ve toplamı 21'den küçük iken bu işlemleri uygular
-                        System.out.println("\nDevam mı tamam mı?"); //Daha fazla kart isteyip istemediğini sorar
-                        b = scan.nextBoolean(); //Oyuncunun cevabını alır
-                        if (b){ //Eğer değer "true" ise
+                        System.out.println("\nDevam, tamam, bölmek ister misiniz? 1,2,3"); //Daha fazla kart isteyip istemediğini sorar
+                        b = scan.nextInt(); //Oyuncunun cevabını alır
+                        if (b==1){ //Eğer değer "true" ise
                             a = rand.nextInt(13);   //13 olmasının sebebi f arrayinin 13 elemanının olması
                             user.add(f[a]);                 //f arrayindeki a'nıncı elemanı alır ve oyuncunun kartlarına ekler
                             System.out.println(user);       //Oyuncuya kartlarını gösterir
@@ -41,8 +42,17 @@ import java.util.Random;
                                 t += integer;               //Oyuncunun kartlarını toplar
                             }
                         }
-                        if (!b){    //Eğer değer "false" ise
+                        if (b==2){    //Eğer değer "false" ise
                             break;  //Döngüyü kırar
+                        }
+                        if (b==3){
+                            if (user.get(0) == user.get(1) & user.size()==2){
+                                user2.add(user.get(1));
+                                user.remove(1);
+                                System.out.println("İlk eliniz :\n" + user + "İkinci eliniz :\n" + user2);
+                            }else {
+                                System.out.println("Şuanda kartlarınızı bölemezsiniz.");
+                            }
                         }
                     }
                     t = 0;
@@ -53,7 +63,7 @@ import java.util.Random;
                     for (Integer integer : bank){
                         y += integer;   //Bankanın kartlarını toplar
                     }
-                    while (bank.size() < 5 & y <= t & y <= 21 & t <= 21) {   //Bankanın 5'ten az kartı var ve kartlarının toplamı 21'den küçük ise devam eder
+                    while (bank.size() < 5 & y <= t & y <= 16 & t <= 21) {   //Bankanın 5'ten az kartı var ve kartlarının toplamı 21'den küçük ise devam eder
                         a = rand.nextInt(13);   //13 olmasının sebebi f arrayinin 13 elemanının olması
                         bank.add(f[a]);     //f arrayindeki a'nıncı elemanı alır ve kurpiyerin eline ekler
                         y = 0;  //Kartların toplamının düzgün sayılabilmesi için
@@ -72,6 +82,9 @@ import java.util.Random;
                         money += 2*betm;
                     } else if (t < y){//Banka 21'e oyuncudan daha yakınsa
                         System.out.println("Kurpiyer önde olduğu için kaybettiniz!");
+                    } else if (t == y){
+                        System.out.println("Berabere");
+                        money += betm;
                     }
                     System.out.println("Tekrar oynamak ister misiniz?");
                     g = scan.nextBoolean();
--- Roulette/src/Main.java
+++ Roulette/src/Main.java
@@ -1,71 +1,136 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> user = new ArrayList<>();
-        ArrayList<Integer> bank = new ArrayList<>();
+        Random number = new Random();
         Scanner scan = new Scanner(System.in);
-        Random rand = new Random();
-        int a;
-        boolean b;
-        int t;
-        int y = 0;
-        int[] f = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10};
-        for (int i = 0; i < 2; i++) {
-            a = rand.nextInt(13);
-            user.add(f[a]);
-            a = rand.nextInt(13);
-            bank.add(f[a]);
-        }
-        System.out.println("Sizin eliniz      : " + user.getFirst() + " " + user.get(1));
-        System.out.println("Kurpiyerin eliniz : " + bank.getFirst() + " * ");
-        while (user.size() < 5) {
-            System.out.println("\nDevam mı tamam mı?");
-            b = scan.nextBoolean();
-            if (b) {
-                a = rand.nextInt(13);
-                user.add(f[a]);
-                System.out.println(user);
-                t = 0;
-                for (Integer integer : user) {
-                    t += integer;
-                    if (21 < t) {
-                        System.out.println("Eliniz 21'i geçtiği için kaybettiniz!");
+        int money = 1000;
+        boolean a = true;
+
+        int[] first = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36};//red
+        int[] second = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35};//green
+        int[] third = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18};//first half
+        int[] fourth = {19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36};//second half
+        int[] fifth = {1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34};//first line
+        int[] sixth = {2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35};//second line
+        int[] seventh = {3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36};//third line
+        int[] eighth = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};//first twelfth
+        int[] nineth = {13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24};//second twelfth
+        int[] tenth = {25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36};//third twelfth
+
+        while (a){
+
+        int winnerNumber = number.nextInt(0, 37);
+
+        System.out.println("How much money do you want to bet?");
+        int betMoney = scan.nextInt();
+        money -= betMoney;
+
+        System.out.println("In where you want to bet? \n 1 Red \n 2 Green \n 3 First Half \n 4 Second Half \n 5 First Line \n 6 Second Line \n 7 Third Line \n 8 First Twelve \n 9 Second Twelve \n 10 Third Twelve \n Or enter 11 if u want to play on a number");
+        int betGroup = scan.nextInt();
+
+        switch (betGroup){
+            case 1:
+                for (int i = 0; i < 18; i++){
+                    if (first[i] == winnerNumber){
+                        money += 2*betMoney;
+                        System.out.println("You won, money : " + money);
                         break;
                     }
                 }
-            }
-            if (!b){
+                break;
+            case 2:
+                for (int i = 0; i < 18; i++){
+                    if (second[i] == winnerNumber){
+                        money += 2*betMoney;
+                        System.out.println("You won, money : " + money);
+                        break;
+                    }
+                }
+                break;
+            case 3:
+                for (int i = 0; i < 18; i++){
+                    if (third[i] == winnerNumber){
+                        money += 2*betMoney;
+                        System.out.println("You won, money : " + money);
+                        break;
+                    }
+                }
+                break;
+            case 4:
+                for (int i = 0; i < 18; i++){
+                    if (fourth[i] == winnerNumber){
+                        money += 2*betMoney;
+                        System.out.println("You won, money : " + money);
+                        break;
+                    }
+                }
+                break;
+            case 5:
+                for (int i = 0; i < 12; i++){
+                    if (fifth[i] == winnerNumber){
+                        money += 3*betMoney;
+                        System.out.println("You won, money : " + money);
+                        break;
+                    }
+                }
+                break;
+            case 6:
+                for (int i = 0; i < 12; i++){
+                    if (sixth[i] == winnerNumber){
+                        money += 3*betMoney;
+                        System.out.println("You won, money : " + money);
+                        break;
+                    }
+                }
+            case 7:
+                for (int i = 0; i < 12; i++){
+                    if (seventh[i] == winnerNumber){
+                        money += 3*betMoney;
+                        System.out.println("You won, money : " + money);
+                        break;
+                    }
+                }
+            case 8:
+                for (int i = 0; i < 12; i++){
+                    if (eighth[i] == winnerNumber){
+                        money += 3*betMoney;
+                        System.out.println("You won, money : " + money);
+                        break;
+                    }
+                }
+                break;
+            case 9:
+                for (int i = 0; i < 12; i++){
+                    if (nineth[i] == winnerNumber){
+                        money += 3*betMoney;
+                        System.out.println("You won, money : " + money);
+                        break;
+                    }
+                }
+                break;
+            case 10:
+                for (int i = 0; i < 12; i++){
+                    if (tenth[i] == winnerNumber){
+                        money += 3*betMoney;
+                        System.out.println("You won, money : " + money);
+                        break;
+                    }
+                }
+                break;
+            case 11:
+                System.out.println("Enter the number you want to bet on");
+                int betNumber = scan.nextInt();
+                if (betNumber==winnerNumber){
+                    money += 36*betMoney;
+                    System.out.println("You won, money : " + money);
+                }
                 break;
             }
-        }
-        t = 0;
-        for (Integer integer : user) {
-            t += integer;
-        }
-        for (Integer integer : bank) {
-            y += integer;
-        }
-        while (bank.size()<5 & y<=t){
-            a = rand.nextInt(13);
-            bank.add(f[a]);
-            y = 0;
-            for (Integer integer : bank) {
-                y += integer;
-            }
-        }
 
-        System.out.println("Kurpiyer kart çeker... \n" + bank);
-
-        if (21<t){
-            System.out.println("Eliniz 21'i geçtiği için kaybettiniz!");
-        } else if (y<t) {
-            System.out.println("Önde olduğunuz için kazandınız!");
-        } else if (21<y) {
-            System.out.println("Banka patladığı için kazandınız!");
-        } else if (t<y) {
-            System.out.println("Kurpiyer önde olduğu için kaybettiniz!");
+            System.out.println("Do you want to play again");
+            a = scan.nextBoolean();
         }
     }
 }
